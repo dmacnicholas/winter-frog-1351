bb6dd95b0a58c9daeab6e2bcdd95a88f9fad9a35
Drew MacNicholas <dmacnicholas@gmail.com>
2022-08-08 11:48:22 -0500

    Add routes for garden show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :plots, only: [:index]
 
   delete '/plots/:plot_id/plants/:plant_id', to: 'plot_plants#destroy'
+
+  resources :gardens, only: [:show]
 end

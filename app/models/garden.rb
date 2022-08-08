0521be4a2a287a48017d8bda6a8243fd3faadfca
Drew MacNicholas <dmacnicholas@gmail.com>
2022-08-08 12:38:03 -0500

    Add hundred days method

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,4 +2,10 @@ class Garden < ApplicationRecord
   has_many :plots
   has_many :plot_plants, through: :plots
   has_many :plants, through: :plot_plants
+
+
+  def harvest_below_one_hundred
+    plants.where('days_to_harvest < ?', 100).distinct
+  end
+
 end

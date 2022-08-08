0afff48022558207b96e4284d66622819605cfae
Drew MacNicholas <dmacnicholas@gmail.com>
2022-08-08 11:17:28 -0500

    Add test to remove plant from  plot

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -37,6 +37,36 @@ RSpec.describe "Plots Index Page" do
    end
 
    expect(page).to_not have_content('Carrot')
+ end
 
-  end
+ it 'displays a link to delete a plant from the plot' do
+   turing_garden = Garden.create!(name: 'Turing Community Garden', organic: true)
+   library_garden = Garden.create!(name: 'Public Library Garden', organic: true)
+
+   plot_1 = Plot.create!(number: 25, size: "Large", direction: "East", garden_id: turing_garden.id)
+   plot_2 = Plot.create!(number: 26, size: "Small", direction: "West", garden_id: turing_garden.id)
+   plot_3 = Plot.create!(number: 2, size: "Small", direction: "South", garden_id: library_garden.id)
+
+   plant_1 = Plant.create!(name: 'Tomato', description: 'Juicy', days_to_harvest: 23)
+   plant_2 = Plant.create!(name: 'Lettuce', description: 'Cripsy', days_to_harvest: 33)
+   plant_3 = Plant.create!(name: 'Cucumber', description: 'Delicious', days_to_harvest: 43)
+   plant_4 = Plant.create!(name: 'Carrot', description: 'Orange', days_to_harvest: 53)
+
+
+   plot_plant_1 = PlotPlant.create!(plot_id: plot_1.id, plant_id: plant_1.id )
+   plot_plant_2 = PlotPlant.create!(plot_id: plot_2.id, plant_id: plant_2.id )
+   plot_plant_3 = PlotPlant.create!(plot_id: plot_3.id, plant_id: plant_3.id )
+
+   visit "/plots"
+
+   within "#plot-#{plot_1.id}" do
+     expect(page).to have_link('Remove Plant')
+     click_on 'Remove Plant'
+   end
+
+   expect(current_path).to eq("/plots")
+
+   within "#plot-#{plot_1.id}" do
+     expect(page).to_not have_content('Tomato')
+   end
 end
